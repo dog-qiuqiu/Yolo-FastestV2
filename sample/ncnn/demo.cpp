b27b667a8c6e79e8003d9265cfecaa9a40e4bc2e
qiuqiu <qiuqiu@example.com>
2021-08-23 23:18:11 +0800

    update ncnn sample code

--- sample/ncnn/demo.cpp
+++ sample/ncnn/demo.cpp
@@ -1,7 +1,20 @@
 #include "yolo-fastestv2.h"
 
 int main()
-{   yoloFastestv2 api;
+{   
+    static const char* class_names[] = {
+        "person", "bicycle", "car", "motorcycle", "airplane", "bus", "train", "truck", "boat", "traffic light",
+        "fire hydrant", "stop sign", "parking meter", "bench", "bird", "cat", "dog", "horse", "sheep", "cow",
+        "elephant", "bear", "zebra", "giraffe", "backpack", "umbrella", "handbag", "tie", "suitcase", "frisbee",
+        "skis", "snowboard", "sports ball", "kite", "baseball bat", "baseball glove", "skateboard", "surfboard",
+        "tennis racket", "bottle", "wine glass", "cup", "fork", "knife", "spoon", "bowl", "banana", "apple",
+        "sandwich", "orange", "broccoli", "carrot", "hot dog", "pizza", "donut", "cake", "chair", "couch",
+        "potted plant", "bed", "dining table", "toilet", "tv", "laptop", "mouse", "remote", "keyboard", "cell phone",
+        "microwave", "oven", "toaster", "sink", "refrigerator", "book", "clock", "vase", "scissors", "teddy bear",
+        "hair drier", "toothbrush"
+    };
+    
+    yoloFastestv2 api;
 
     api.loadModel("./model/yolo-fastestv2-opt.param",
                   "./model/yolo-fastestv2-opt.bin");
@@ -15,6 +28,25 @@ int main()
         std::cout<<boxes[i].x1<<" "<<boxes[i].y1<<" "<<boxes[i].x2<<" "<<boxes[i].y2
                  <<" "<<boxes[i].score<<" "<<boxes[i].cate<<std::endl;
         
+        char text[256];
+        sprintf(text, "%s %.1f%%", class_names[boxes[i].cate], boxes[i].score * 100);
+
+        int baseLine = 0;
+        cv::Size label_size = cv::getTextSize(text, cv::FONT_HERSHEY_SIMPLEX, 0.5, 1, &baseLine);
+
+        int x = boxes[i].x1;
+        int y = boxes[i].y1 - label_size.height - baseLine;
+        if (y < 0)
+            y = 0;
+        if (x + label_size.width > cvImg.cols)
+            x = cvImg.cols - label_size.width;
+
+        cv::rectangle(cvImg, cv::Rect(cv::Point(x, y), cv::Size(label_size.width, label_size.height + baseLine)),
+                      cv::Scalar(255, 255, 255), -1);
+
+        cv::putText(cvImg, text, cv::Point(x, y + label_size.height),
+                    cv::FONT_HERSHEY_SIMPLEX, 0.5, cv::Scalar(0, 0, 0));
+
         cv::rectangle (cvImg, cv::Point(boxes[i].x1, boxes[i].y1), 
                        cv::Point(boxes[i].x2, boxes[i].y2), cv::Scalar(255, 255, 0), 2, 2, 0);
     }
